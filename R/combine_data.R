36fa4be3d2d485394de78fa9665e7ede971e6679
Coen Eisma <coeneisma@gmail.com>
2025-04-15 14:02:34 +0200

    Removed unused functions

--- R/combine_data.R
+++ R/combine_data.R
@@ -1,28 +1,3 @@
-
-#' Vind datumvelden in een dataframe
-#'
-#' @param df Een dataframe
-#'
-#' @return Een character vector met namen van datumvelden
-#'
-#' @keywords internal
-find_date_fields <- function(df) {
-  date_field_patterns <- c(
-    "^BEGINDATUM", "^EINDDATUM", "^STARTDATUM", "^OPHEFFINGSDATUM",
-    "^INGANGSDATUM", "^INBEDRIJFDATUM", "^UITBEDRIJFDATUM",
-    "_PERIODE$"
-  )
-
-  # Find columns matching the patterns
-  date_fields <- character(0)
-  for (pattern in date_field_patterns) {
-    matching_cols <- grep(pattern, names(df), value = TRUE)
-    date_fields <- c(date_fields, matching_cols)
-  }
-
-  return(unique(date_fields))
-}
-
 #' Verkrijg de directory voor het opslaan van RIO package data
 #'
 #' @return Een character string met het pad naar de RIO data directory
